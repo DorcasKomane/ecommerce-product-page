2e834acb7b0ddb4d615c7894f586171cb999f5a7
Dorcas Komane <dorcaskomane1@gmail.com>
2023-08-30 17:01:26 +0200

    Added lightbox effect.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,33 @@
-window.onload = function(){
-    window.addEventListener('scroll', function(e){
-        if(window.pageYOffset > 100){
-            this.document.querySelector('header').classList.add('is-scrolling');
-        }else {
-            this.document.querySelector('header').classList.remove('is-scrolling');
-        }
-    });
+// window.onload = function(){
+//     window.addEventListener('scroll', function(e){
+//         if(window.pageYOffset > 100){
+//             this.document.querySelector('header').classList.add('is-scrolling');
+//         }else {
+//             this.document.querySelector('header').classList.remove('is-scrolling');
+//         }
+//     });
 
     const menuBtn = document.querySelector('.hamburger');
     const mobileNav = document.querySelector('#mobileNav');
+    const cartIcon = document.querySelector('#cartIcon');
+    const shoppingCart = document.querySelector('#shoppingCart');
+
 
     menuBtn.addEventListener('click', function(){
         menuBtn.classList.toggle('is-active');
         menuBtn.style.zIndex = '5';
         mobileNav.style.display = 'block';
     });
+
+    cartIcon.addEventListener('mouseover', function(){
+        shoppingCart.style.display = 'block';
+    });
+
+    
+// }
+
+function myFunction(thumbnail){
+    const currentImage = document.getElementById('currentImage');
+    currentImage.src = thumbnail.src;
+    currentImage.style.width = '500px';
 }
