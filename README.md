7f4e88f308ed733d67379553670905601f9973f9
Dorcas Komane <dorcaskomane1@gmail.com>
2023-09-05 19:28:54 +0200

    Updated README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ecommerce-product-page
 Solution to Frontend Mentor eCommerce Product Page Coding Challenge
+
+
+## Author
+
+- Website - [Dorcas Komane](https://dorcas-portfolio-site.netlify.app/)
+- Frontend Mentor - [@DorcasKomane](https://www.frontendmentor.io/profile/DorcasKomane)
+- Twitter - [@DorcasInTech](https://www.twitter.com/DorcasInTech)
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,3 @@
-// window.onload = function(){
-//     window.addEventListener('scroll', function(e){
-//         if(window.pageYOffset > 100){
-//             this.document.querySelector('header').classList.add('is-scrolling');
-//         }else {
-//             this.document.querySelector('header').classList.remove('is-scrolling');
-//         }
-//     });
 
     const menuBtn = document.querySelector('.hamburger');
     const mobileNav = document.querySelector('#mobileNav');
